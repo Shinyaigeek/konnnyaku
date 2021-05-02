59b44bbfa82b44fe92a4ef2858f3c1a28c4a7a87
shinyaigeek <philispaxil@gmail.com>
2021-05-02 15:17:21 +0900

    Chore: init packages/server

---
+++ examples/SimpleServer/src/main.rs
@@ -0,0 +1,7 @@
+use konnnyaku_server::server::server::{Server};
+
+fn main() {
+    let server = Server {};
+    Server::echo();
+    println!("Hello, world!");
+}
---
+++ packages/konnnyaku-server/src/server/server.rs
@@ -0,0 +1,9 @@
+pub struct Server {
+
+}
+
+impl Server {
+    pub fn echo() {
+        println!("jey");
+    }
+}
