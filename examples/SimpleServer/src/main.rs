7c5fa1b7714993ad03b86d77b865ad8b6ea15e78
shinyaigeek <philispaxil@gmail.com>
2021-05-02 17:11:36 +0900

    Feature: establish tcp connection

--- examples/SimpleServer/src/main.rs
+++ examples/SimpleServer/src/main.rs
@@ -2,5 +2,5 @@ use konnnyaku_server::server::server::Server;
 
 fn main() {
     let server = Server::build(3000);
-    println!("Hello, world!");
+    server.serve();
 }
--- packages/konnnyaku-server/src/server/server.rs
+++ packages/konnnyaku-server/src/server/server.rs
@@ -1,11 +1,26 @@
+use std::net::TcpListener;
+use std::io::{Write, Read};
+
 pub struct Server {
-    port: i32
+    port: i32,
+    listener: TcpListener,
 }
 
 impl Server {
     pub fn build(port: i32) -> Self {
-        Self {
-            port
+        let listener = TcpListener::bind(format!("127.0.0.1:{}", port.to_string())).unwrap();
+        Self { port, listener }
+    }
+
+    pub fn serve(self) {
+        for stream in self.listener.incoming() {
+            println!("Connection from {}", stream.peer_addr().unwrap());
+            let mut buffer = [0; 1024];
+            loop {
+                let nbytes = stream.read(&mut buffer).unwrap();
+                stream.write(&buffer[..nbytes]).unwrap();
+                stream.flush().unwrap();
+            }
         }
     }
 }
