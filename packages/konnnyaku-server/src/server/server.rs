8b4e8dadf71ee86407b903b1a8804cdae0877ce3
shinyaigeek <philispaxil@gmail.com>
2021-05-02 17:21:41 +0900

    Feature: to_string tcp stream

--- packages/konnnyaku-server/src/server/server.rs
+++ packages/konnnyaku-server/src/server/server.rs
@@ -1,5 +1,6 @@
 use std::net::TcpListener;
 use std::io::{Write, Read};
+use std::str;
 
 pub struct Server {
     port: i32,
@@ -14,10 +15,14 @@ impl Server {
 
     pub fn serve(self) {
         for stream in self.listener.incoming() {
+            let mut stream = stream.unwrap();
             println!("Connection from {}", stream.peer_addr().unwrap());
             let mut buffer = [0; 1024];
             loop {
                 let nbytes = stream.read(&mut buffer).unwrap();
+                println!("bytes:{:?}", buffer);
+                let http = str::from_utf8(&buffer).unwrap();
+                println!("http : {:?}", http);
                 stream.write(&buffer[..nbytes]).unwrap();
                 stream.flush().unwrap();
             }
