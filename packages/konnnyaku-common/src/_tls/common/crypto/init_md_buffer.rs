c04d7f19ecd112bae0984224e95503abb53404b4
Shinyaigeek <Philispaxil@gmail.com>
2022-03-27 18:29:53 +0900

    Feature: init MD buffer

---
+++ packages/konnnyaku-common/src/_tls/common/crypto/init_md_buffer.rs
@@ -0,0 +1,7 @@
+fn init_md_buffer() -> (u32, u32, u32, u32) {
+    let a: u32 = 0x_67_45_23_01;
+    let b: u32 = 0x_ef_cd_ab_89;
+    let c: u32 = 0x_98_ba_dc_fe;
+    let d: u32 = 0x_10_32_54_76;
+    return (a, b, c, d);
+}
--- packages/konnnyaku-common/src/_tls/common/crypto/mod.rs
+++ packages/konnnyaku-common/src/_tls/common/crypto/mod.rs
@@ -1,3 +1,4 @@
 mod append_length;
+mod init_md_buffer;
 mod md5;
 mod pad;
